9f1d5710f959d2998e09d2355f23d801fe11d14b
Dan Taylor <dan@logicalgenetics.com>
2017-06-08 10:53:47 +0100

    Changed query to scan to get around timeout issue in lambda. No idea why that works

--- lambda/MoodBotResults.js
+++ lambda/MoodBotResults.js
@@ -6,35 +6,35 @@ let channel_id;
 
 var docClient = new AWS.DynamoDB.DocumentClient();
 
-Array.prototype.countBy = function (key) {
-    return this.reduce(function (rv, x) {
-        rv[x[key]] = (rv[x[key]] || 0) + 1;
-        return rv;
-    }, {});
+Array.prototype.countBy = function(key) {
+  return this.reduce(function(rv, x) {
+    rv[x[key]] = (rv[x[key]] || 0) + 1;
+    return rv;
+  }, {});
 };
 
-Date.prototype.getWeek = function () {
+Date.prototype.getWeek = function() {
     var onejan = new Date(this.getFullYear(), 0, 1);
     return Math.ceil((((this - onejan) / 86400000) + onejan.getDay() + 1) / 7);
 };
 
-Date.prototype.previousWeek = function () {
+Date.prototype.previousWeek = function() {
     return new Date(this.getTime() - (7 * 24 * 60 * 60 * 1000));
 };
 
 function forWeek(week) {
     var expression = "#wk = :week";
     var attrs = { ":week": week };
-    if (channel_id) {
+    if(channel_id) {
         expression += " and channel_id = :chan";
         attrs[":chan"] = channel_id;
     }
-
+    
     return {
-        TableName: "MoodResponses",
+        TableName : "MoodResponses",
         IndexName: 'week-channel_id-index',
-        KeyConditionExpression: expression,
-        ExpressionAttributeNames: {
+        FilterExpression: expression,
+        ExpressionAttributeNames:{
             "#wk": "week"
         },
         ExpressionAttributeValues: attrs
@@ -43,7 +43,7 @@ function forWeek(week) {
 
 function handleError(err, callback) {
     console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
-    callback(null, { "error": JSON.stringify(err, null, 2) });
+    callback(null, {"error": JSON.stringify(err, null, 2)});
 }
 
 function happinessMetric(moods) {
@@ -51,21 +51,21 @@ function happinessMetric(moods) {
     var bad = moods.bad || 0;
     var awesome = moods.awesome || 0;
     var terrible = moods.terrible || 0;
-
+    
     var score = good + (bad * -1) + (awesome * 5) + (terrible * -5);
     var count = good + bad + awesome + terrible + (moods.meh || 0);
-
+    
     return Math.round(score * 100 / count);
 }
 
 function overallMood(happiness) {
-    if (happiness > 200)
+    if(happiness > 200)
         return "awesome";
-    else if (happiness < -200)
+    else if(happiness < -200)
         return "terrible";
-    else if (happiness > 50)
+    else if(happiness > 50)
         return "good";
-    else if (happiness < -50)
+    else if(happiness < -50)
         return "bad";
     else
         return "meh";
@@ -73,7 +73,7 @@ function overallMood(happiness) {
 
 function handleData(data, callback, week) {
     console.log("Query succeeded.");
-
+            
     var moods = data.Items.countBy("mood");
     var happiness = happinessMetric(moods);
     var results = {
@@ -88,13 +88,13 @@ function handleData(data, callback, week) {
 }
 
 function runFor(date, tries, callback) {
-    var week = date.getWeek();
+    var week = date.getWeek();    
     console.log('Fetching mood results for week: ' + week);
-
-    docClient.query(forWeek(week), function (err, data) {
+    
+    docClient.scan(forWeek(week), function(err, data) {
         if (err) {
             handleError(err, callback);
-        } else if (data.Items.length > 0 || tries <= 0) {
+        } else if(data.Items.length > 0 || tries <= 0) {
             handleData(data, callback, week);
         } else {
             runFor(date.previousWeek(), tries - 1, callback);
@@ -102,8 +102,8 @@ function runFor(date, tries, callback) {
     });
 }
 
-exports.handler = function (event, context, callback) {
+exports.handler = function(event, context, callback) {
     channel_id = event.channel_id;
-
+    
     runFor(new Date(), 1, callback);
 };
